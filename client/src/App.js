3115a8ae235e58f698cb0b908c7612f0f6dae8ba
markizenlee <xc2097@nyu.edu>
2025-10-11 05:27:16 -0400

    Simple Web App - React Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/api/message")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Red-trees test web app.</h1>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 import express from 'express';
-const app = express();
-const PORT = 3000;
+import path from "path";
+import { fileURLToPath } from "url";
 
-app.get('/', (req, res) => {
-    res.send('Red-trees test page');
+const app = express();
+const PORT = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from Express backend!" });
 });
 
-app.listen(PORT);
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
