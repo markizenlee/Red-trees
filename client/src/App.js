1dcc7d2a14849881d738fc472468716294b980a4
markizenlee <xc2097@nyu.edu>
2025-10-13 03:19:21 -0400

    Header Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./Header";
 import sampleImage from "./assets/sample.png";
 
 function App() {
@@ -27,17 +28,22 @@ function App() {
   }, []);
 
   return (
+    <>
+      <Header />
       <div>
         <section className="hero">
-          <h1 className="text">
-            [Red Trees Description Text Goes Here.]
-          </h1>
+          <p className="text">
+            As a leader in the world of fashion accessories, RED TREES is dedicated to creating a dreamland of daily wear for consumers of all styles and lifestyles. 
+We deeply understand the desire for self-expression and the pursuit of diverse life experiences. Through our ingenious designs, we bring to life every individual’s boundless imagination of fashion. 
+Here, every customer can effortlessly create daily inspiration and shine with their own unique brilliance.
+          </p>
         </section>
 
         <section className="image-section">
           <img src={sampleImage} alt="Sample Image" className="full-image" />
         </section>
       </div>
+    </>
   );
 }
 
---
+++ client/src/Header.js
@@ -0,0 +1,24 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="top-row">
+                <a href="#">Contact Us</a>
+                <a href="#">Services</a>
+            </div>
+
+            <div className="middle-row">
+
+            </div>
+
+            <div className="bottom-row">
+                <a href="#">Brand</a>
+                <a href="#">Product</a>
+                <a href="#">Stores</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
