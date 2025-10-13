5436843312d0f24a151c32712ac2bd27f445db55
markizenlee <xc2097@nyu.edu>
2025-10-13 07:14:25 -0400

    Hero Section Layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import "./Home.css";
+import heroImage from "../assets/home-hero.png";
 
 function Home() {
     return (
@@ -6,14 +7,14 @@ function Home() {
             <section className="hero">
                 <div className="text-container">
                     <p className="text">
-                        text
+                        As a leader in the world of fashion accessories, RED TREES is dedicated to creating a dreamland of daily wear for consumers of all styles and lifestyles. <br /> <br />
+                        We deeply understand the desire for self-expression and the pursuit of diverse life experiences. Through our ingenious designs, we bring to life every individual’s boundless imagination of fashion. <br /> <br />
+                        Here, every customer can effortlessly create daily inspiration and shine with their own unique brilliance.
                     </p>
                 </div>
                 
                 <div className="image-container">
-                    <p className="text">
-                        text
-                    </p>
+                    <img src={heroImage} alt="hero image" className="image" />
                 </div>
             </section>
         </div>
