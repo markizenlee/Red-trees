7abfb6600ff1127ad49c390b165945ce66b62e6d
markizenlee <xc2097@nyu.edu>
2025-10-15 06:48:46 -0400

    Adjust Server to commonJS: adjust to node 10

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require('express');
+const path = require('path');
+// const { fileURLToPath } = require('url');
 
 const app = express();
 const PORT = 80;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from Express backend!" });
