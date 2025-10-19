0536f53d533bd5a86e20a669ab72f1d5c91e879e
markizenlee <xc2097@nyu.edu>
2025-10-19 08:10:07 -0400

    Fix server.js version issues

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.get("/api/message", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
+app.get("/*path", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
