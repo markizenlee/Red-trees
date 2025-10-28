077f56a09425f9c73a6fd2163d5eaedd9017cc3f
markizenlee <xc2097@nyu.edu>
2025-10-28 04:37:59 -0400

    Store Page (PC)

--- client/src/pages/stores/DubaiStore.js
+++ client/src/pages/stores/DubaiStore.js
@@ -6,8 +6,94 @@ import storeD05Image from "../../assets/store-D05.jpg";
 
 function DubaiStore() {
     return (
-        <>
-        </>
+        <div>
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Dubai Burjuman Center
+                    </p>
+
+                    <p className="text">
+                        First Floor <br /><br />
+                        TEL: (0455) 247 86
+                    </p>
+                </div>
+
+                <div className="image-container">
+                    <img src={storeD01Image} alt="store Dubai 01 image" className="image" />
+                </div>
+            </section>
+
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Dubai Al Ghurair Center
+                    </p>
+
+                    <p className="text">
+                        Ground Floor <br /><br />
+                        TEL: (0422) 166 88
+                    </p>
+                </div>
+
+                <div className="image-container">
+                    <img src={storeD02Image} alt="store Dubai 02 image" className="image" />
+                </div>
+            </section>
+
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Dubai Arabian Center
+                    </p>
+
+                    <p className="text">
+                        Ground Floor <br /><br />
+                        TEL: (0422) 844 14
+                    </p>
+                </div>
+
+                <div className="image-container">
+                    <img src={storeD03Image} alt="store Dubai 03 image" className="image" />
+                </div>
+            </section>
+
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Dubai Dragon Mart 1
+                    </p>
+
+                    <p className="text">
+                        Ground Floor <br />
+                        Shop number DHG06 <br /><br />
+                        TEL: (0455) 836 98
+                    </p>
+                </div>
+
+                <div className="image-container">
+                    <img src={storeD04Image} alt="store Dubai 04 image" className="image" />
+                </div>
+            </section>
+
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Dubai Mall
+                    </p>
+
+                    <p className="text">
+                        Level 2 <br />
+                        Way to Metro Station <br /><br />
+                        TEL: (0444) 569 67
+                    </p>
+                </div>
+
+                <div className="image-container">
+                    <img src={storeD05Image} alt="store Dubai 05 image" className="image" />
+                </div>
+            </section>
+        </div>
     );
 }
 
--- client/src/pages/stores/SharjahStore.js
+++ client/src/pages/stores/SharjahStore.js
@@ -4,8 +4,58 @@ import storeS03Image from "../../assets/store-S03.jpg";
 
 function SharjahStore() {
     return (
-        <>
-        </>
+        <div>
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Sharjah Sahara Center
+                    </p>
+            
+                    <p className="text">
+                        Ground Floor <br /><br />
+                        TEL: (0544) 942 915
+                    </p>
+                </div>
+            
+                <div className="image-container">
+                    <img src={storeS01Image} alt="store Sharjah 01 image" className="image" />
+                </div>
+            </section>
+
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Sharjah City Center
+                    </p>
+            
+                    <p className="text">
+                        First Floor <br /><br />
+                        TEL: (0655) 222 88
+                    </p>
+                </div>
+            
+                <div className="image-container">
+                    <img src={storeS02Image} alt="store Sharjah 02 image" className="image" />
+                </div>
+            </section>
+
+            <section className="content">
+                <div className="text-container">
+                    <p className="title">
+                        Sharjah Mega Mall
+                    </p>
+            
+                    <p className="text">
+                        Ground Floor <br /><br />
+                        TEL: (0655) 000 18
+                    </p>
+                </div>
+            
+                <div className="image-container">
+                    <img src={storeS03Image} alt="store Sharjah 03 image" className="image" />
+                </div>
+            </section>
+        </div>
     );
 }
 
