451ef58796a5d3d318183a7a7208719029aab1ee
markizenlee <xc2097@nyu.edu>
2025-10-27 21:56:04 -0400

    Store Page Prep

--- client/src/pages/Stores.js
+++ client/src/pages/Stores.js
@@ -1,15 +1,18 @@
 import "./Stores.css";
 
+import storeD01Image from "../assets/store-D01.jpg";
+import storeD02Image from "../assets/store-D02.jpg";
+import storeD03Image from "../assets/store-D03.jpg";
+import storeD04Image from "../assets/store-D04.jpg";
+import storeD05Image from "../assets/store-D05.jpg";
+import storeS01Image from "../assets/store-S01.jpg";
+import storeS02Image from "../assets/store-S02.jpg";
+import storeS03Image from "../assets/store-S03.jpg";
+
 function Stores() {
     return (
         <div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <h1>Stores Page</h1>
+            
         </div>
     );
 }
