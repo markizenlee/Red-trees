dfc0d186eca95bfda06c312faa95411169797334
markizenlee <xc2097@nyu.edu>
2026-05-26 18:50:18 +0800

    Contact Us Page Prototype

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -2,8 +2,50 @@ import "./ContactUs.css";
 
 function ContactUs() {
     return (
-        <div>
-            
+        <div className="contact-us-page">
+            <section className="hero">
+                <div className="text-area">
+                    <div className="heading-container">
+                        <span className="title">
+                            Get in Touch
+                        </span>
+
+                        <p className="text">
+                            We'd love to hear from you. Whether you have questions about our jewelry collections, store information, or partnership inquiries, please don't hesitate to reach out.
+                        </p>
+                    </div>
+
+                    <div className="text-container">
+                        <span className="title">
+                            EMAIL US
+                        </span>
+
+                        <p className="text">
+                            redtreesuae@hotmail.com
+                        </p>
+                    </div>
+
+                    <div className="text-container">
+                        <span className="title">
+                            CALL US
+                        </span>
+
+                        <p className="text">
+                            []
+                        </p>
+                    </div>
+
+                    <div className="text-container">
+                        <span className="title">
+                            FOLLOW US
+                        </span>
+
+                        <p className="text">
+                            @redtrees_jewelry
+                        </p>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
