ac7e72a7cf3ba356bb9ba542ee8bc561538ba400
markizenlee <xc2097@nyu.edu>
2026-07-13 01:25:13 +0800

    Implemented Contact Form Full Layout and Styling

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -10,7 +10,40 @@ function ContactForm() {
 
     return (
         <form className="contact-form">
-            
+            <div className="info-section">
+                <div className="info-group">
+                    <label htmlFor="name">Name:</label>
+                    <input 
+                        id="name" 
+                    />
+                </div>
+
+                <div className="info-group">
+                    <label htmlFor="email">E-Mail:</label>
+                    <input 
+                        id="email" 
+                    />
+                </div>
+
+                <div className="info-group">
+                    <label htmlFor="phone">Phone (Optional):</label>
+                    <input 
+                        id="phone" 
+                    />
+                </div>
+            </div>
+            <div className="message-section">
+                <div className="message-group">
+                    <label htmlFor="message">Message:</label>
+                    <textarea 
+                        id="message"
+                    />
+                </div>
+
+                <div className="submit-group">
+                    <button type="submit">Send Message</button>
+                </div>
+            </div>
         </form>
     );
 }
