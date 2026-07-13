43d0362ef4f56789790c848171ec19eeed338992
markizenlee <xc2097@nyu.edu>
2026-07-13 20:52:12 +0800

    Connected form components with React state values stored ready to be delivered

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -8,35 +8,68 @@ function ContactForm() {
         message: ""
     });
 
+    function handleChange(evt) {
+        const { id, value } = evt.target;
+
+        setForm((prevForm) => ({
+            ...prevForm,
+            [id]: value
+        }));
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        console.log(form);
+    }
+
     return (
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={handleSubmit}>
             <div className="info-section">
                 <div className="info-group">
                     <label htmlFor="name">Name:</label>
                     <input 
-                        id="name" 
+                        id="name"
+                        type="text"
+                        value={form.name}
+                        onChange={handleChange}
+                        placeholder="Your Name"
+                        required
                     />
                 </div>
 
                 <div className="info-group">
                     <label htmlFor="email">E-Mail:</label>
                     <input 
-                        id="email" 
+                        id="email"
+                        type="email"
+                        value={form.email}
+                        onChange={handleChange}
+                        placeholder="youremail@gmail.com"
+                        required
                     />
                 </div>
 
                 <div className="info-group">
                     <label htmlFor="phone">Phone (Optional):</label>
                     <input 
-                        id="phone" 
+                        id="phone"
+                        type="tel"
+                        value={form.phone}
+                        onChange={handleChange}
+                        placeholder="04-2342600"
                     />
                 </div>
             </div>
+            
             <div className="message-section">
                 <div className="message-group">
                     <label htmlFor="message">Message:</label>
                     <textarea 
                         id="message"
+                        value={form.message}
+                        onChange={handleChange}
+                        placeholder="How can we help you?"
+                        required
                     />
                 </div>
 
