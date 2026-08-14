f551a847a32871cd337c2e24c378e051a0137beb
markizenlee <xc2097@nyu.edu>
2026-08-15 05:12:01 +0800

    build product page shell and category navigation

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Link, useLocation } from "react-router-dom";
 
 import "./Product.css";
 
@@ -6,21 +6,42 @@ import ProductCategory from "./product/ProductCategory";
 import productCategories from "./product/productData";
 
 function Product() {
+    const location = useLocation();
+    const activeCategoryId = location.pathname.split("/")[2];
+
     return (
         <div className="product">
-            <Routes>
-                <Route index element={<Navigate to="rings" replace />} />
+            <nav className="product-navigation" aria-label="Product categories">
+                <ul>
+                    {productCategories.map(category => (
+                        <li key={category.id}>
+                            <Link
+                                to={`/product/${category.id}`}
+                                className={activeCategoryId === category.id ? "active" : ""}
+                                aria-current={activeCategoryId === category.id ? "page" : undefined}
+                            >
+                                {category.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
 
-                {productCategories.map(category => (
-                    <Route
-                        key={category.id}
-                        path={category.id}
-                        element={<ProductCategory category={category} />}
-                    />
-                ))}
+            <main className="product-content">
+                <Routes>
+                    <Route index element={<Navigate to="rings" replace />} />
 
-                <Route path="*" element={<Navigate to="rings" replace />} />
-            </Routes>
+                    {productCategories.map(category => (
+                        <Route
+                            key={category.id}
+                            path={category.id}
+                            element={<ProductCategory category={category} />}
+                        />
+                    ))}
+
+                    <Route path="*" element={<Navigate to="rings" replace />} />
+                </Routes>
+            </main>
         </div>
     );
 }
--- client/src/pages/product/ProductCategory.js
+++ client/src/pages/product/ProductCategory.js
@@ -1,8 +1,23 @@
 function ProductCategory({ category }) {
     return (
-        <div data-category={category.id}>
-            {category.name}
-        </div>
+        <section
+            className="product-category"
+            data-category={category.id}
+            aria-labelledby={`${category.id}-heading`}
+        >
+            <header className="product-category-header">
+                <h1 id={`${category.id}-heading`}>
+                    {category.name}
+                </h1>
+
+                <p>
+                    {category.introduction}
+                </p>
+            </header>
+
+            <div className="product-category-products">
+            </div>
+        </section>
     );
 }
 
